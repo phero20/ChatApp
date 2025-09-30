b3247b783116a63efbddf38a79f810ff0e3055c2
Feroz <mdferozahmed27156@gmail.com>
2025-09-30 09:45:23 +0530

    4

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-     
-   </>
+    <>
+    <p className="text-blue-600"> hellllllllo</p>
+    <Button>hellllllllllllo</Button>
+    
+    </>
   );
 }
